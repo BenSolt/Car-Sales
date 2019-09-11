aefaa1369140932ddc4a95543baaffdc581f1339
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-09-11 14:54:52 -0400

    wrapped App in Provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//Import Create Store
+import { createStore } from "redux";
+//Import Provider
+import { Provider } from "react-redux";
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//const for Store and mock reducer
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, 
+rootElement);
